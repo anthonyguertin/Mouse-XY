9c2cdf7303729a847dd39309d6ec05491771bc25
guertin <anthony.guertin@gmail.com>
2016-10-05 23:54:05 -0600

    Get Mouse-XY version 2.2

--- content_script.js
+++ content_script.js
@@ -1,15 +1,57 @@
-var clicks = 0;   
-document.addEventListener('click', getClickPosition, false);  
-       function getClickPosition(e) {
-                  //reload the page if clicks are greater than 1
-       		if(clicks>0){
-          		window.location.reload()
-                  //get mouse coordinates and print them to the screen 
-       		}else {
-       			var xPosition = e.clientX; 
-       			var yPosition = e.clientY; 
-       			alert('X : ' + String(xPosition) + '  Y : ' + String(yPosition)); 
-       			clicks +=1;
-       } 
+/** 
+ * Chrome Extension: Mouse-XY
+ * Author: Anthony Guertin
+ * Version: 2.2
+ */
 
-       };
+const NEW_LINE = "\n";
+const EMPTY_STRING = "";
+
+var clicks = 0;   
+var cordArray = [];
+var doRecord = false;
+var message = EMPTY_STRING;
+
+document.addEventListener('click', getClickPosition, false);
+
+function forEachCordinate(current, index, cordArray)
+{
+	var click_index = index + 1;
+	message += String(click_index) + ') X : ' + String(current.x) + '  Y : ' + String(current.y) + '\n';
+}
+
+
+function getClickPosition(e) {
+	// Reload the page if clicks are greater than 1
+	if (clicks > 1 && !doRecord) {
+		window.location.reload()
+		return;  
+	}
+
+	// Get mouse co-ordinates and print them to the screen 
+	var xPosition = e.clientX; 
+	var yPosition = e.clientY;
+	
+	if (doRecord || clicks == 0) {
+		cordArray.push
+		(
+			{
+				x: xPosition,
+				y: yPosition
+			}
+		);
+	}
+	clicks +=1;
+
+	message = EMPTY_STRING;
+	message += 'Total Number of Clicks: ' + String(clicks) + NEW_LINE;
+	cordArray.forEach(forEachCordinate);
+	var cont = window.confirm ('Continue Clicks? ' + NEW_LINE + message);
+
+	if (cont) 
+	{
+			doRecord = true;
+			return; 
+	}
+	doRecord = false;
+};
